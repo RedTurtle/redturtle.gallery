4ff3f3bcabbb44bcc136d501512d3cad4cb2d605
nzambello <nicola.zambello@redturtle.it>
2017-09-28 15:53:20 +0200

    added view for modal: methods and template

--- src/redturtle/gallery/browser/gallery.py
+++ src/redturtle/gallery/browser/gallery.py
@@ -5,5 +5,25 @@ from Products.Five.browser import BrowserView
 class GalleryView(FolderView, BrowserView):
     """
     Gallery view
-        self.request.form
     """
+
+
+class GalleryModal(BrowserView):
+    """
+    Gallery modal helper method for slick carousel
+    """
+
+    contents = []
+    itemIndex = -1
+
+    def getContents(self):
+        self.contents = self.context.listFolderContents(
+                            contentFilter={"portal_type": "Image"}
+                        )
+        return self.contents
+
+    def getIndex(self):
+        self.itemIndex = [x.UID() for x in self.contents].index(
+                            self.request.form['image']
+                         )
+        return self.itemIndex
