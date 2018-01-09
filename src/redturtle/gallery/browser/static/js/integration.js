20557a3e7c4a533fff5a8ca9b6da588b3e33b933
Piero Nicolli <piero.nicolli@redturtle.it>
2018-01-09 10:57:31 +0100

    Fixed js algorithm (async recursion)

--- src/redturtle/gallery/browser/static/js/integration.js
+++ src/redturtle/gallery/browser/static/js/integration.js
@@ -1,12 +1,59 @@
 require(['jquery'], function($) {
   'use strict';
 
+  function init() {
+    var inputs = $('.gallery-modal-wrapper').find(
+      'select, input, textarea, button:not(.gallery-modal-close), a'
+    );
+    var firstInput = inputs.first();
+    var lastInput = inputs.last();
+    var closeInput = $('.gallery-modal-close').first();
+
+    lastInput.on('keydown', function(e) {
+      if (e.which === 9) {
+        if (!e.shiftKey) {
+          e.preventDefault();
+          closeInput.focus();
+        }
+      }
+    });
+
+    firstInput.on('keydown', function(e) {
+      if (e.which === 9) {
+        if (e.shiftKey) {
+          e.preventDefault();
+          closeInput.focus();
+        }
+      }
+    });
+
+    closeInput.on('click', function() {
+      $('.plone-modal-close').click();
+    });
+
+    closeInput.on('keydown', function(e) {
+      if (e.which === 9) {
+        e.preventDefault();
+
+        if (e.shiftKey) {
+          lastInput.focus();
+        } else {
+          firstInput.focus();
+        }
+      }
+    });
+
+    setTimeout(function() {
+      $('.gallery-slider .gallery-item.slick-active').focus();
+    }, 500);
+  }
+
   function checkSliderLoaded() {
     if (
       $('.gallery-slider').length &&
       $('.gallery-slider').hasClass('slick-initialized')
     ) {
-      return true;
+      init();
     } else {
       setTimeout(checkSliderLoaded, 200);
     }
@@ -14,52 +61,7 @@ require(['jquery'], function($) {
 
   $(document).on('ready', function() {
     $('body').on('init', '.gallery-slider', function() {
-      if (checkSliderLoaded()) {
-        var inputs = $('.gallery-modal-wrapper').find(
-          'select, input, textarea, button:not(.gallery-modal-close), a'
-        );
-        var firstInput = inputs.first();
-        var lastInput = inputs.last();
-        var closeInput = $('.gallery-modal-close').first();
-
-        lastInput.on('keydown', function(e) {
-          if (e.which === 9) {
-            if (!e.shiftKey) {
-              e.preventDefault();
-              closeInput.focus();
-            }
-          }
-        });
-
-        firstInput.on('keydown', function(e) {
-          if (e.which === 9) {
-            if (e.shiftKey) {
-              e.preventDefault();
-              closeInput.focus();
-            }
-          }
-        });
-
-        closeInput.on('click', function() {
-          $('.plone-modal-close').click();
-        });
-
-        closeInput.on('keydown', function(e) {
-          if (e.which === 9) {
-            e.preventDefault();
-
-            if (e.shiftKey) {
-              lastInput.focus();
-            } else {
-              firstInput.focus();
-            }
-          }
-        });
-
-        setTimeout(function() {
-          $('.gallery-slider .gallery-item.slick-active').focus();
-        }, 500);
-      }
+      checkSliderLoaded();
     });
   });
 });
