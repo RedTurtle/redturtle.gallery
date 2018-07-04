f9763ebf36ded67c9accab4b9de561e56661e4bb
Piero Nicolli <piero.nicolli@redturtle.it>
2018-07-04 13:15:13 +0200

    Back to development: 2.0.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='redturtle.gallery',
-    version='2.0.3',
+    version='2.0.4.dev0',
     description="Plone addon that adds a gallery view with a carousel made with slick.",  # noqa
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
